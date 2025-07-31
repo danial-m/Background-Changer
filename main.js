26ce2d3b0f68cc136f1d524b66bb3052d7423732
Mutshidzi Mukwevho <132754483+danial-m@users.noreply.github.com>
2025-07-31 10:43:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function yellow(){
+    document.body.style.background="yellow";
+}
+
+function Red(){
+    document.body.style.background="Red";
+}
+
+function Blue(){
+    document.body.style.background="Blue";
+}
+
+function White(){
+    document.body.style.background="White";
+}
+
+function Purple(){
+    document.body.style.background="Purple";
+}
